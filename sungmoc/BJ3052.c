41a2213b4583d0f09d3c37bd3e151767903bc90e
sungmoc <usungmo0317@gmail.com>
2022-03-21 16:30:16 +0900

    3052번 어느정도 풀어봄

--- sungmoc/BJ3052.c
+++ sungmoc/BJ3052.c
@@ -2,5 +2,76 @@
 #include <stdio.h>
 
 int main() {
-    printf("test");
+    int fn=0,i=0, arr[9]={0,}, sum=0, arr1[9]={0,},n1=0;
+    
+    for (i=0;i<10;i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (i=0;i<10;i++) {
+        sum=42%arr[i];
+        arr1[i]=sum;
+    }
+    for (i=0;i<10;i++) {
+        if (i==0) {
+            n1=arr1[i];
+            if (arr[i] != n1) {
+                fn+=1;
+            }
+        }
+        else if (i==1) {
+            if (arr[i] != n1) {
+                fn+=1;
+                n1=arr1[i];
+            } 
+        }
+        else if (i==2) {
+            if (arr[i] != n1) {
+                fn+=1;
+                n1=arr1[i];
+            }
+        }
+        else if (i==3) {
+            if (arr[i] != n1) {
+                fn+=1;
+                n1=arr1[i];
+            }
+        }
+        else if (i==4) {
+            if (arr[i] != n1) {
+                fn+=1;
+                n1=arr1[i];
+            }
+        }
+        else if (i==5) {
+            if (arr[i] != n1) {
+                fn+=1;
+                n1=arr1[i];
+            }
+        }
+        else if (i==6) {
+            if (arr[i] != n1) {
+                fn+=1;
+                n1=arr1[i];
+            }
+        }
+        else if (i==7) {
+            if (arr[i] != n1) {
+                fn+=1;
+                n1=arr1[i];
+            }
+        }
+        else if (i==8) {
+            if (arr[i] != n1) {
+                fn+=1;
+                n1=arr1[i];
+            }
+        }
+        else {
+            if (arr[i] != n1) {
+                fn+=1;
+                n1=arr1[i];
+            }
+        }
+    }
+    printf("%d", fn);
 }
