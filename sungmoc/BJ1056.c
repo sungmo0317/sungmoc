b055fca1d370531d52e4fd4378ac7aa24c96b6ca
sungmoc <usungmo0317@gmail.com>
2022-03-30 09:37:38 +0900

    1056번 2차수정

--- sungmoc/BJ1056.c
+++ sungmoc/BJ1056.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 
 int hansu(a) {
-    int i=0,d1=0,d2=0,d3=0,d4=0,sum=0,namuji=0,mok=0,deungcha(4)={0,},jaritsu=0,ic=0,output=0;
+    int i=0,d1=0,d2=0,d3=0,d4=0,sum=0,namuji=0,mok=0, deungcha[4]={0,},jaritsu=0,ic=0,output=0;
     
     for (i=0;i<=a;i++) {
         ic=i;
@@ -13,16 +13,16 @@ int hansu(a) {
             jaritsu=jaritsu+1;
         }
         for (i=0; i<=jaritsu; i++) {
-            sum=i;
-            namuji=sum%10;
-            mok=sum/10;
-            deungcha(i)=namuji;
+            mok=i;
+            namuji=mok%10;
+            mok=mok/10;
+            deungcha[i]=namuji;
         }
         if (jaritsu<=2) {
             output=output+1;
         }
         else if (jaritsu==3) {
-            if (deungcha[i]-deungcha[i+1]==deungcha[i+1]-deungcha[i+2]) {
+            if (deungcha[0]-deungcha[1]==deungcha[1]-deungcha[2]) {
                 output=output+1;
             }
         }
@@ -33,7 +33,8 @@ int hansu(a) {
 
 
 int main() {
-    int a=0;
-    hansu("%d",&a);
-    printf("%d", a);
+    int a=0, sum=0;
+    scanf("%d", &a);
+    sum=hansu(a);
+    printf("%d", sum);
 }
