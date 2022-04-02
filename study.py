3e9c9c314c3e9a1241c04eb110b1e439c0425feb
sungmoc <usungmo0317@gmail.com>
2022-04-02 10:26:50 +0900

    한수놈.. 거의 다풀었다

--- study.py
+++ study.py
@@ -1,4 +1,2 @@
-n1,m1,n2,m2=1,12,3,16
-p=(m2-m1)/(n2-n1)
-x=16-3*p
-print(x)
+a=False
+print(a)
--- sungmoc/BJ1056.c
+++ sungmoc/BJ1056.c
@@ -4,9 +4,9 @@
 #include <stdbool.h>
 
 int hansu(a) {
-    int i=0,j=0,d1=0,d2=0,d3=0,d4=0,sum=0,namuji=0,mok=0, deungcha[4]={0,},jaritsu=0,ic=0,output=0;
+    int i=1,j=0,d1=0,d2=0,d3=0,d4=0,sum=0,namuji=0,mok=0, deungcha[4]={0,},jaritsu=0,ic=0,output=0;
     
-    for (i=1;i<=a;i++) {
+    while (i<=a) {
         ic=i;
         jaritsu=0;
         while(ic!=0) {
@@ -14,28 +14,31 @@ int hansu(a) {
             jaritsu=jaritsu+1;
         }
         for (j=1; j<=jaritsu; j++) {
-            mok=i;
+            if(j==1) {
+                mok=i;
+            }
             namuji=mok%10;
             mok=mok/10;
-            deungcha[i]=namuji;
+            deungcha[j]=namuji;
         }
         if (jaritsu<=2) {
             output=output+1;
         }
         else if (jaritsu==3) {
-            if (deungcha[2]-deungcha[1]==deungcha[1]-deungcha[0]) {
+            if ((deungcha[2]-deungcha[1])==(deungcha[1]-deungcha[0])) {
                 output=output+1;
             }
         }
-        
+        i=i+1;
     }
     return output;
+    
 }
 
 
 int main() {
     int a=0, sum=0;
     scanf("%d", &a);
-    //sum=hansu(a);
+    //printf("hello");
     printf("%d",hansu(a));
 }
